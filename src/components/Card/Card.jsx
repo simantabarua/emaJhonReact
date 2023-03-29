7375c2e5ebf09034c691ec57e04a1f411c427f93
simantabarua <simanta.barua@yahoo.com>
2023-03-29 23:43:34 +0600

    cart

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,22 +1,29 @@
 import React from "react";
 
 const Card = (props) => {
-  console.log(props.product);
-  const {name, img, price, } = props.product;
-  
+  const { name, img, price } = props.product;
+
   return (
     <>
-    <div className="card w-96 bg-base-100 shadow-xl">
-  <figure>
-    <img src={img}  className="w-full h-64 object-cover" alt="Shoes" /></figure>
-  <div className="card-body">
-    <h2 className="card-title">{name}</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-ghost bg-orange-400">Buy Now</button>
-    </div>
-  </div>
-</div>
+      <div className="card w-80 bg-base-100 shadow-xl">
+        <figure>
+          <img src={img} className="w-full h-64 object-cover" alt="Shoes" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{name}</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button
+              className="btn btn-ghost bg-orange-400"
+              onClick={() => {
+                props.handleCart(props.product);
+              }}
+            >
+              Buy Now
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -6,6 +6,7 @@ const Shop = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [cart, setCart] = useState([]);
   useEffect(() => {
     const productLoad = async () => {
       try {
@@ -19,11 +20,17 @@ const Shop = () => {
     };
     productLoad();
   }, []);
+
+  const handleCart = (product) => {
+    let newCart = [...cart, product];
+    setCart(newCart);
+  };
+
   if (loading) {
-   return <div className="text-4xl text-center p-10">Loading...</div>;
+    return <div className="text-4xl text-center p-10">Loading...</div>;
   }
   if (error) {
-   return <div>Error: {error.message}</div>;
+    return <div>Error: {error.message}</div>;
   }
   return (
     <div className="grid grid-cols-5 text-center ">
@@ -31,12 +38,13 @@ const Shop = () => {
         <h2 className="text-4xl">Product: {products.length} </h2>
         <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 p-2 gap-3">
           {products.map((product, index) => (
-           <Card key={index} product={product} />
+            <Card key={index} product={product} handleCart={handleCart} />
           ))}
         </div>
       </div>
       <div className="col-span-1 bg-orange-400 ">
-      <h2 className="text-3xl font-bold text-blue-600">Order Summary</h2>
+        <h2 className="text-3xl font-bold text-blue-600">Order Summary</h2>
+        <p>Item total : {cart.length} </p>
       </div>
     </div>
   );
