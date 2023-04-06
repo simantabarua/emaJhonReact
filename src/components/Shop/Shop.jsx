e8de8c96f965c78431069e2128dbd11f096f04db
simantabarua <webdevelopersimanta@gmail.com>
2023-04-06 20:09:43 +0600

    local storage added

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,76 +1,58 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import loadData from "../../api/dataLoad";
 import Card from "../Card/Card";
+import Cart from "../Cart/Cart";
 
 const Shop = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [price, setPrice] = useState(0);
-  const [shipping, setShipping] = useState(0);
-  const [cart, setCart] = useState(() => {
-    const storedCart = localStorage.getItem("cart");
-    return storedCart ? JSON.parse(storedCart) : [];
-  });
-  
+
   useEffect(() => {
     const productLoad = async () => {
       try {
         const products = await loadData();
         setProducts(products);
+        setLoading(false);
       } catch (error) {
         setError(error);
-      } finally {
         setLoading(false);
       }
     };
     productLoad();
   }, []);
 
-  const handleCart = useCallback(
-    (product) => {
-      let newCart = [...cart, product];
-      setCart(newCart);
-      setPrice(price + product.price);
-      setShipping(shipping + product.shipping);
-      localStorage.setItem("cart", JSON.stringify(newCart));
-    },
-    [cart, price, shipping]
-  );
-  const [tax, setTax] = useState(0);
-  const [total, setTotal] = useState(0);
+  const [carts, setCarts] = useState([]);
 
+  const handleAddToCart = (product) => {
+    const productInCart = carts.find((item) => item.id === product.id);
+    if (!productInCart) {
+      const newCart = [...carts, product];
+      setCarts(newCart);
+      localStorage.setItem("cartItems", JSON.stringify(newCart));
+    }
+  };
   useEffect(() => {
-    const tax = price * 0.07;
-    const total = price + shipping + tax;
-    setTax(tax.toFixed(2));
-    setTotal(total);
-  }, [price, shipping]);
+    const cartItems = JSON.parse(localStorage.getItem("cartItems") || "[]");
+    setCarts(cartItems);
+  }, []);
 
- 
-  if (loading) {
-    return <div className="text-4xl text-center p-10">Loading...</div>;
-  }
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
   return (
-    <div className="grid grid-cols-5 text-center ">
-      <div className="col-span-4 bg-gray-100">
-        <h2 className="text-4xl">Product: {products.length} </h2>
-        <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 p-2 gap-3">
-          {products.map((product, index) => (
-            <Card key={index} product={product} handleCart={handleCart} />
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-1">
+      <div className=" md:col-span-3">
+        <h1>Total product Fond: {products.length}</h1>
+        <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-2 place-items-center">
+          {products.map((product) => (
+            <Card
+              key={product.id}
+              product={product}
+              handleAddToCart={handleAddToCart}
+            ></Card>
           ))}
         </div>
       </div>
-      <div className="col-span-1 bg-orange-400 sticky top-0 h-96 text-left p-5  ">
-        <h2 className="text-3xl font-bold text-blue-600">Order Summary</h2>
-        <p>Item total : $ {cart.length} </p>
-        <p>Price Total: $ {price}</p>
-        <p>Shipping Total: $ {shipping}</p>
-        <p>Tax Total: $ {tax}</p>
-        <p>Grand Total: $ {total}</p>
+      <div className=" md:col-span-1">
+        <Cart carts={carts} />
       </div>
     </div>
   );
